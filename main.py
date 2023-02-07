17506ccfc76ddeaa21f3c184b0d9a72cd7ee4332
Guilherme Souza <gss214@hotmail.com>
2023-02-06 21:27:25 -0300

    feat: cifração e decifração com OAEP
    Co-authored-by: Maria Eduarda Holanda <dudaholandah@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from src.rsa import RSA
+from src.generate_prime import GeneratePrime
 
 def main():
     rsa = RSA()
     public_key, private_key = rsa.generate_keys()
+    opa = rsa.OAEPencrypt("aaaaaaaaaaaaaaaaaaa!", "", public_key)
+    print(opa)
+    print()
+    pao = rsa.OAEPdecrypt(opa, '', private_key)
+    print(pao)
+    """
     print(f"Public key: {public_key}")
     print()
     print(f"Private key: {private_key}")
+    """
 
 if __name__ == "__main__":
     main()
